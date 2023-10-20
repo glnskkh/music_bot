cc7c95abcd93cf40ad529039f9f5d1e8ee7860fc
Georgii Glinskikh <glnskkh@vk.com>
2023-10-20 16:38:06 +0500

    test are parametrized now

--- src/test/java/org/musicbotcom/ServerTest.java
+++ src/test/java/org/musicbotcom/ServerTest.java
@@ -2,48 +2,42 @@ package org.musicbotcom;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class ServerTest {
 
+  private final int millisecondsTimeout = 100;
   private Server testServer;
 
-  @Test
-  void testHelp() throws InterruptedException {
-    var help = new TestIO("/help");
-
-    testServer = new Server(help);
-
-    new Thread(() -> testServer.run()).start();
-
-    Thread.sleep(1000);
-
-    testServer.stop();
-
-    assertEquals("Это бот позволяет переносить аудиозаписи из вконтакте прямиком в диалог с ботом\n"
-        + "Помимо этого бот также умеет находить треки близкие по жанрам, а также по звучанию\n"
-        + "Перечень команд:\n" + "<...>\n<...>\n<...>\n", help.answer);
-  }
-
-
-  @Test
-  void testStart() throws InterruptedException {
-    var start = new TestIO("/start");
-
-    testServer = new Server(start);
-
-    new Thread(() -> testServer.run()).start();
-
-    Thread.sleep(1000);
-
-    testServer.stop();
-
-    assertEquals("Привет!\n"
+  private static Stream<Arguments> provideIOCommands() {
+    return Stream.of(Arguments.of("/help",
+        "Это бот позволяет переносить аудиозаписи из вконтакте прямиком в диалог с ботом\n"
+            + "Помимо этого бот также умеет находить треки близкие по жанрам, а также по звучанию\n"
+            + "Перечень команд:\n" + "<...>\n<...>\n<...>\n"), Arguments.of("/start", "Привет!\n"
         + "Это бот позволяет переносить аудиозаписи из вконтакте прямиком в диалог с ботом\n"
         + "Помимо этого бот также умеет находить треки близкие по жанрам, а также по звучанию\n"
         + "Для привязки своего профиля ВК, отправьте боту <сслыка на бота в вк> код:\n"
         + "<копируемое сообщение>\n\n"
-        + "После привязки, бот ВКонтакте оставит инструкцию по использованию бота\n", start.answer);
+        + "После привязки, бот ВКонтакте оставит инструкцию по использованию бота\n"));
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideIOCommands")
+  void testIO(String command, String expected) throws InterruptedException {
+    var testIO = new TestIO(command);
+
+    testServer = new Server(testIO);
+
+    new Thread(() -> testServer.run()).start();
+
+    Thread.sleep(millisecondsTimeout);
+
+    testServer.stop();
+
+    assertEquals(expected, testIO.answer);
   }
 
   static class TestIO implements IO {
